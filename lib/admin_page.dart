b8536d35dd2bec6073dba453d0e62a820987d688
amfunNyLORD <funnybulletgames@yandex.ru>
2023-07-07 13:17:36 +0300

    Сверстал главное меню admin_page, но без выпадающего меню

---
+++ lib/admin_page.dart
@@ -0,0 +1,401 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget adminPage() {
+  Map<String, String> objects = {
+    'Йошкар-Ола, бульвар Ураева, 4-157': 'Васильев Антон Николаевич',
+    'Йошкар-Ола, улица Мира, 68-24': 'Васильев Антон Николаевич',
+    'Чебоксары, проспект Победы, 18-95': 'Евсютин Олег Максимович',
+    'Йошкар-Ола, улица Машиностроителей, 32-47': 'Леухин Степан Анатольевич'
+  };
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[
+        _header(),
+        _menu(),
+        _searchBar(),
+        _objects(objects),
+      ],
+    ),
+  );
+}
+
+Widget _header() {
+  return Padding(
+    padding: const EdgeInsets.only(top: 30, left: 10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _icon(),
+        _user(),
+        _logout(),
+      ],
+    ),
+  );
+}
+
+Image _icon() {
+  return Image.asset(
+    'assets/images/logo.png',
+    scale: 2,
+  );
+}
+
+Row _user() {
+  return Row(
+    children: <Widget>[
+      _userImage(),
+      Padding(padding: const EdgeInsets.only(left: 8), child: _userInfo()),
+    ],
+  );
+}
+
+Image _userImage() {
+  return Image.asset(
+    'assets/images/userImage.png',
+    scale: 2,
+  );
+}
+
+Column _userInfo() {
+  return Column(
+    children: <Widget>[
+      _userName(),
+      Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: _userPost(),
+      ),
+    ],
+  );
+}
+
+Text _userName() {
+  return const Text(
+    'Иванов Максим',
+    style: TextStyle(
+      fontSize: 12.0,
+      color: Colors.red,
+    ),
+  );
+}
+
+Text _userPost() {
+  return const Text(
+    'администратор',
+    style: TextStyle(
+      fontSize: 9.0,
+      color: Colors.grey,
+    ),
+  );
+}
+
+ElevatedButton _logout() {
+  return ElevatedButton(
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      shadowColor: Colors.transparent,
+    ),
+    child: Row(
+      children: <Widget>[
+        const Text(
+          'Выйти',
+          style: TextStyle(
+            fontSize: 9.0,
+            color: Colors.black,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: Image.asset(
+            'assets/images/logout-icon.png',
+            scale: 2,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _menu() {
+  return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Container(
+        height: 30,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(187, 14, 42, 1.0),
+              Color.fromRGBO(105, 0, 0, 1.0),
+            ],
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              // как убрать левый паддинг у иконки
+              padding: const EdgeInsets.all(0.0),
+              width: 100,
+              height: 20,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add_outlined,
+                  size: 20,
+                ),
+                label: const Text(
+                  'Добавить объект',
+                  style: TextStyle(fontSize: 5),
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                    backgroundColor: Colors.transparent,
+                    elevation: 0.0,
+                    shadowColor: Colors.transparent,
+                    side: const BorderSide(
+                      width: 0.5,
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                  shadowColor: Colors.transparent,
+                ),
+                child: const Text(
+                  'Переданные объекты',
+                  style: TextStyle(
+                    fontSize: 5,
+                  ),
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                  shadowColor: Colors.transparent,
+                ),
+                child: const Text(
+                  'Сохраненные объекты',
+                  style: TextStyle(fontSize: 5),
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                  shadowColor: Colors.transparent,
+                ),
+                child: const Text(
+                  'Завершенные объекты',
+                  style: TextStyle(fontSize: 5),
+                )),
+          ],
+        ),
+      ));
+}
+
+Widget _searchBar() {
+  return Padding(
+      padding: const EdgeInsets.only(left: 13, top: 15),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 230,
+            height: 30,
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Color.fromRGBO(174, 42, 39, 1.0),
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: SvgPicture.asset(
+              'assets/images/filter-icon.svg',
+              width: 20,
+              height: 20,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 5),
+            child: Text(
+              'сортировать по',
+              style: TextStyle(fontFamily: 'Roboto', fontSize: 5),
+            ),
+          ),
+          const DropdownButtonExample(),
+        ],
+      ));
+}
+
+const List<String> filters = <String>['One', 'Two', 'Three', 'Four'];
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = filters.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: filters.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+Widget _objects(Map<String, String> objects) {
+  return customObjects(objects);
+}
+
+class ListStructure {
+  String key;
+  String value;
+
+  ListStructure(this.key, this.value);
+}
+
+Widget customObjects(Map<String, String> objects) {
+  List<ListStructure> list = [];
+  objects.forEach((key, value) => list.add(ListStructure(key, value)));
+
+  List<TableRow> rows = [];
+  rows.add(TableRow(
+      decoration: BoxDecoration(
+          border: Border.all(
+        width: 0.5,
+      )),
+      children: [
+        Container(
+            width: 150,
+            height: 30,
+            decoration: BoxDecoration(
+                border: Border.all(
+              width: 0.5,
+            )),
+            child: const Padding(
+              padding: EdgeInsets.only(left: 5, top: 7),
+              child: Text(
+                'Адрес',
+                style: TextStyle(color: Color.fromRGBO(174, 42, 39, 1.0)),
+              ),
+            )),
+        Container(
+            width: 150,
+            height: 30,
+            decoration: BoxDecoration(
+                border: Border.all(
+              width: 0.5,
+            )),
+            child: const Padding(
+              padding: EdgeInsets.only(left: 5, top: 7),
+              child: Text(
+                'Прораб',
+                style: TextStyle(color: Color.fromRGBO(174, 42, 39, 1.0)),
+              ),
+            )),
+        Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(border: Border.all(width: 0.5)),
+        ),
+        Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(border: Border.all(width: 0.5)),
+        ),
+      ]));
+
+  for (int i = 0; i < 4; i++) {
+    rows.add(TableRow(
+        decoration: BoxDecoration(
+            border: Border.all(
+          width: 0.5,
+        )),
+        children: [
+          Container(
+              width: 150,
+              height: 30,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 0.5,
+                  )),
+              child: Center(
+                child: Text(
+                  list[i].key,
+                  style: const TextStyle(color: Colors.black, fontSize: 6),
+                ),
+              )),
+          Container(
+              width: 150,
+              height: 30,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 0.5,
+                  )),
+              child: Center(
+                child: Text(
+                  list[i].value,
+                  style: const TextStyle(color: Colors.black, fontSize: 8),
+                ),
+              )),
+          Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(border: Border.all(width: 0.5)),
+            child: const Icon(Icons.edit),
+          ),
+          Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(border: Border.all(width: 0.5)),
+            child: const Icon(Icons.delete),
+          ),
+        ]));
+  }
+  return Padding(
+      padding: const EdgeInsets.only(right: 6),
+      child: Table(columnWidths: const {
+        0: IntrinsicColumnWidth(),
+        1: IntrinsicColumnWidth(),
+        2: IntrinsicColumnWidth(),
+        3: IntrinsicColumnWidth(),
+      }, children: rows));
+}
